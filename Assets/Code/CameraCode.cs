29626483ee0541b83c9f9f461cd2b3ec48e0a98f
LoadUpStudios <31973893+BrandonT80@users.noreply.github.com>
2022-03-30 12:06:11 -0500

    Scenes and Camera Changes
    ^

--- Assets/Code/CameraCode.cs
+++ Assets/Code/CameraCode.cs
@@ -8,6 +8,16 @@ public class CameraCode : MonoBehaviour
     public GameObject[] cameraLocations = new GameObject[13];
     private int cameraIterator = 1;
     public double distance = 0;
+    public GameObject blackJackLoc;
+    public GameObject blackJackLookLoc;
+    public GameObject slotsLoc;
+    public GameObject slotsLookLoc;
+    private bool blackJack = false;
+    private bool slots = false;
+    private GameObject gameLoc;
+    private GameObject gameLookLoc;
+    private bool doMovement = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,23 +27,65 @@ public class CameraCode : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        distance = Vector3.Distance(this.transform.position, cameraLocations[cameraIterator].transform.position);
-        if (distance < 1)
+        if (doMovement)
         {
-            if(cameraIterator < 12)
+            if (!blackJack && !slots)
             {
-                cameraIterator++;
+                distance = Vector3.Distance(this.transform.position, cameraLocations[cameraIterator].transform.position);
+                if (distance < 1)
+                {
+                    if (cameraIterator < 12)
+                    {
+                        cameraIterator++;
+                    }
+                    else
+                    {
+                        cameraIterator = 0;
+                    }
+                    this.transform.rotation = cameraLocations[cameraIterator].transform.rotation;
+                }
+                else
+                {
+                    this.transform.position = Vector3.Lerp(this.transform.position, cameraLocations[cameraIterator].transform.position, (1.0f / ((float)distance + 1.5f)) * Time.deltaTime);
+                }
             }
             else
             {
-                cameraIterator = 0;
+                distance = Vector3.Distance(this.transform.position, gameLoc.transform.position);
+                if (distance < 0.01)
+                {
+                    doMovement = false;
+                    Debug.Log("Done");
+                    //this.transform.rotation = gameLoc.transform.rotation;
+                }
+                else
+                {
+                    //this.transform.LookAt(gameLoc.transform);
+                    this.transform.LookAt(gameLookLoc.transform);
+                    //this.transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(this.transform.position, blackJackTableLoc.transform.position, 0.01f, 0.01f));
+                    //this.transform.position = Vector3.RotateTowards(transform.forward, gameLoc.transform.position, 1.0f, 1.0f);
+                    this.transform.position = Vector3.Lerp(this.transform.position, gameLoc.transform.position, (1.0f * Time.deltaTime));
+                    
+                }
             }
-            this.transform.rotation = cameraLocations[cameraIterator].transform.rotation;
         }
-        else
-        {
-            this.transform.position = Vector3.Lerp(this.transform.position, cameraLocations[cameraIterator].transform.position, (1.0f/((float)distance + 1.5f)) * Time.deltaTime);
-        }
+    }
+
+    public void goToBlackJack()
+    {
+        blackJack = true;
+        gameLoc = blackJackLoc;
+        gameLookLoc = blackJackLookLoc;
+        //this.transform.LookAt(gameLoc.transform);
+        //this.transform.rotation = gameLoc.transform.rotation;
+    }
+
+    public void goToSlots()
+    {
+        slots = true;
+        gameLoc = slotsLoc;
+        gameLookLoc = slotsLookLoc;
+        //this.transform.LookAt(gameLoc.transform);
     }
 
 }
