9aa24793650089166225ce1bf21244b8ce204b2c
Tisbet <tisbetanaya@gmail.com>
2022-04-17 19:53:03 -0500

    Added Slot Outcome Functionality

--- Assets/Code/SlotSelectionManager.cs
+++ Assets/Code/SlotSelectionManager.cs
@@ -7,8 +7,8 @@ public class SlotSelectionManager : MonoBehaviour
 {
     [SerializeField] private string selectableTag = "Selectable";
     [SerializeField] private float timer;
-    //[SerializeField] private int speed;
-    public GameObject slot1, slot2, slot3;
+    [SerializeField] private int cost;
+    public GameObject slot1, slot2, slot3, outcome1, outcome2, outcome3, money;
     Random rand = new Random();
 
     // Start is called before the first frame update
@@ -29,10 +29,11 @@ public class SlotSelectionManager : MonoBehaviour
                 var selection = hit.transform;
                 if(selection.CompareTag(selectableTag))
                 {
-                    //Debug.Log("You selected the " + hit.transform.name);
+                    money.GetComponent<MainSceneManager>().spendMoney(cost);
                     StartCoroutine(Spin(slot1, timer - 2, rand.Next(500, 1000)));
                     StartCoroutine(Spin(slot2, timer - 1, rand.Next(500, 1000)));
                     StartCoroutine(Spin(slot3, timer, rand.Next(500, 1000)));
+                    StartCoroutine(CalculateScore(timer + 1));
                 }
             }
         }
@@ -40,15 +41,41 @@ public class SlotSelectionManager : MonoBehaviour
 
     IEnumerator Spin(GameObject slot, float timer, float speed)
     {
-        while(timer > 0)
+        while(timer > 0 || outcome1.GetComponent<Outcome>().colliderList.Count > 2 || outcome2.GetComponent<Outcome>().colliderList.Count > 2 || outcome3.GetComponent<Outcome>().colliderList.Count > 2)
         {
-            Debug.Log("Spinning: " + timer);
-            //slot1.transform.Rotate(0, 0, Time.deltaTime * speed);
             slot.transform.Rotate(Vector3.forward, Time.deltaTime * speed);
-            //slot2.transform.Rotate(Vector3.forward, Time.deltaTime * speed);
-            //slot3.transform.Rotate(Vector3.forward, Time.deltaTime * speed);
             timer -= Time.deltaTime;
             yield return null;
+        }
+    }
+
+    IEnumerator CalculateScore(float time)
+    {
+        Debug.Log("Entered CalculateScore. Waiting...");
+        //wait until slots are done spinning 
+        yield return new WaitForSeconds(time + 1);
+
+        string result1, result2, result3;
+
+        result1 = outcome1.GetComponent<Outcome>().colliderList[0].gameObject.tag;
+        outcome1.GetComponent<Outcome>().colliderList.RemoveAt(0);
+        Debug.Log("Result 1: " + result1);
+
+        result2 = outcome2.GetComponent<Outcome>().colliderList[0].gameObject.tag;
+        outcome2.GetComponent<Outcome>().colliderList.RemoveAt(0);
+        Debug.Log("Result 2: " + result2);
+
+        result3 = outcome3.GetComponent<Outcome>().colliderList[0].gameObject.tag;
+        outcome3.GetComponent<Outcome>().colliderList.RemoveAt(0);
+        Debug.Log("Result 3: " + result3);
+
+        if(string.Equals(result1, result2) && string.Equals(result2, result3))
+        {
+            money.GetComponent<MainSceneManager>().addMoney(15);
+        }
+        else if(string.Equals(result1, result2) || string.Equals(result1, result3) || string.Equals(result2, result3))
+        {
+            money.GetComponent<MainSceneManager>().addMoney(12);
         }
     }
 }
